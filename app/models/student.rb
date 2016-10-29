55660a027b539d255c2470311ad6e88c7fded2db
Andrew Eskenazi <Drewesk@gmail.com>
2016-10-28 18:33:58 -0600

    Added Rails Validations to Fields
    * Phone numbers can’t be left blank, must contain numbers, and must have 10 digits.
    
    * I made the first_name field only accept Andrew Mike Rita and Vanessa as acceptable names.
    
    [#133071785]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,18 @@
 class Student < ApplicationRecord
+  validates :cell_phone, :workphone, :home_phone,
+    :numericality => {
+      :message => 'can only contain numbers'
+    },
+    :length => {
+      :is => 10,
+      :message => 'number must be 10-digits'
+    },
+    :presence => {
+      :message => 'please add a valid phone-number'
+    }
+
+  validates :first_name, inclusion: {
+    :in => %w(andrew mike vanessa rita),
+    :message => '%{value}, is not a valid name'
+  }
 end
